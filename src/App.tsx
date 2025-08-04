3aa0b2cb08b83b3c70dabb7888a43003cf572ab9
unarovshaha <shahaunarov7@gmail.com>
2025-08-04 12:02:35 +0500

    Convert project to Vite React

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import './App.css'
+
+function App() {
+  return (
+    <div className="app">
+      <header className="app-header">
+        <h1>Turon Director App</h1>
+        <p>Welcome to your React application!</p>
+      </header>
+      <main className="app-main">
+        <p>
+          This is your converted Vite React project. You can start building your application here.
+        </p>
+      </main>
+    </div>
+  )
+}
+
+export default App
